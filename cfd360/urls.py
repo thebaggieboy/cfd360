3873e72530fac26b2ff26a2b9c436c0c1949a23b
Enimofe Odujirin <daemonargo@protonmail.com>
2023-02-26 22:20:10 +0100

    middleware

--- cfd360/urls.py
+++ cfd360/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', include('core.urls', namespace='core'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
+urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
