8e0351dcf8fc0560750d2ae2e8dfc71077988d49
Enimofe Odujirin <daemonargo@protonmail.com>
2023-02-26 22:38:34 +0100

    added missing static files

--- cfd360/settings.py
+++ cfd360/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
