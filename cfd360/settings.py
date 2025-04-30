e3d2f8f238ca0ca31b12fe27440cc3575b524174
Mofe Odujirin <thebaggieboy@protonmail.com>
2025-04-30 04:41:26 -0400

    identiy

--- cfd360/settings.py
+++ cfd360/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'cfd360.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-       'default': dj_database_url.config(default='postgres://postgresql://avantrades_user:TQXpzEjSEVGau1zBmzLjjtLNzepOOMAr@dpg-d030e2euk2gs73etvci0-a.oregon-postgres.render.com/avantrades')
+       'default': dj_database_url.config(default='postgres://postgresql://avantrades_7tc9_user:SDYwzZAWhHtp5taMgID1hXJghoBoSnya@dpg-d08u3c3uibrs73cnalhg-a.oregon-postgres.render.com/avantrades_7tc9')
 }
 
 PASSWORD_HASHERS = [
